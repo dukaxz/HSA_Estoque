b4dd21804f8a94c4afa5ba3dcf28446e04fd2ab0
Eduardo <eduardopferreira249@gmail.com>
2026-07-01 22:34:20 -0300

    Criação das funções de listar peças e adicionar peça dentro do banco e tratamento para verificação se esta ou nao em estoque.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-
+use std::str::FromStr;
 
 use rusqlite::Connection;
 
@@ -19,6 +19,27 @@ enum ProximaOperacao {
     Calibrar,
 }
 
+impl FromStr for ProximaOperacao {
+    type Err = String;
+    
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "FormarPonta" =>        Ok(ProximaOperacao::FormarPonta),
+            "Trefilar" =>           Ok(ProximaOperacao::Trefilar),
+            "Endireitar" =>         Ok(ProximaOperacao::Endireitar),
+            "Cortar" =>             Ok(ProximaOperacao::Cortar),
+            "LimparBlank" =>        Ok(ProximaOperacao::LimparBlank),
+            "FormarEndForming" =>   Ok(ProximaOperacao::FormarEndForming),
+            "Curvar" =>             Ok(ProximaOperacao::Curvar),
+            "Recalcar" =>           Ok(ProximaOperacao::Recalcar),
+            "Rebarbar" =>           Ok(ProximaOperacao::Rebarbar),
+            "Zincar" =>             Ok(ProximaOperacao::Zincar),
+            "Calibrar" =>           Ok(ProximaOperacao::Calibrar),
+            _ => Err(format!("Operação desconhecida: {}", s)),
+        }
+    }
+}
+
 #[derive(Debug, Serialize, Deserialize)]
 
 struct Peca {
@@ -33,14 +54,14 @@ struct Peca {
 
 fn iniciar_banco(conn: &Connection) {
     conn.execute_batch(
-        "CREATE A TABLE IF NOT EXISTIS pecas (
+        "CREATE TABLE IF NOT EXISTS pecas (
             id  INTEGER PRIMARY KEY AUTOINCREMENT,
             codigo_pi   TEXT NOT NULL,
             nome    TEXT NOT NULL,
             proxima_operacao TEXT NOT NULL,
             data_entrada TEXT NOT NULL,
             lote    TEXT NOT NULL,
-            data_saida  TEXT,
+            data_saida  TEXT
             
         );"
     ).expect("Erro ao criar tabela");
@@ -51,8 +72,31 @@ fn main() {
 
     iniciar_banco(&conn);
 
-    println!("Banco iniciado com sucesso!");
+    let peca = Peca {
+        id: 0, // o banco vai gerar o id real
+        codigo_pi:        String::from("15570080"),
+        nome:             String::from("31-2298 - Haste Curvada"),
+        proxima_operacao: ProximaOperacao::Recalcar,
+        lote:             String::from("H0501001001"),
+        data_entrada:     String::from("01/07/2026"),
+        data_saida:       None,
+    };
+
+    inserir_peca(&conn, &peca);
+
+    let lista = listar_pecas(&conn);
+    for p in &lista {
+        let status = match &p.data_saida {
+            Some(data) => format!("Saiu em: {}", data),
+            None       => String::from("Em estoque"),
+        };
+        println!("#{} | {} | {} | {} | {} | {}", p.id, p.codigo_pi, p.nome, p.lote, p.data_entrada, status);
+    }
+
 }
+
+    
+
 
 fn inserir_peca(conn: &Connection, peca: &Peca) {
     conn.execute(
@@ -61,10 +105,36 @@ fn inserir_peca(conn: &Connection, peca: &Peca) {
     (
         &peca.codigo_pi,
         &peca.nome,
-        format!("{:?}", peca.proxima_operacao), // converte enum para texto
+        format!("{:?}", peca.proxima_operacao),
         &peca.lote,
         &peca.data_entrada,
         &peca.data_saida,
         )
     ).expect("Erro ao inserir peça");
 }
+
+fn listar_pecas(conn: &Connection) -> Vec<Peca> {
+    
+    let mut stmt = conn
+    .prepare("SELECT id, codigo_pi, nome, proxima_operacao, lote, data_entrada, data_saida FROM pecas").expect("Erro ao preparar consulta");
+
+    let pecas: Vec<Peca> = stmt
+        .query_map([], |row| {
+            Ok(Peca {
+                id:               row.get(0)?,
+                codigo_pi:        row.get(1)?,
+                nome:             row.get(2)?,
+                proxima_operacao: {
+                    let s: String = row.get(3)?;
+                    s.parse().expect("Operação invalida no banco")
+                },
+                lote:             row.get(4)?,
+                data_entrada:     row.get(5)?,
+                data_saida:       row.get(6)?,
+        })
+    })
+    .expect("Erro ao consultar peça").filter_map(|r| r.ok()).collect();
+    
+    pecas
+}   
+
